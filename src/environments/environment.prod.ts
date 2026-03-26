b01fbaadee9b89c160c6c691be239aaa25c31813
PATHAKOTAMEGHASHYAMREDDY <meghashyamreddy014@gmail.com>
2026-03-26 17:00:38 +0530

    error in production updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://graph-ql-d6co.onrender.com' // Replace with your deployed backend URL
+  apiUrl: 'https://graph-ql-d6co.onrender.com/graphql' // Added /graphql endpoint
 };
